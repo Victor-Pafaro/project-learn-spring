bd66a4bc81d866f131a783f76e91b3dbf7ad7a49
Victor-Pafaro <vitor.pafaro@gmailcom>
2024-11-16 00:59:08 -0300

    Project created

---
+++ src/main/java/com/victorpafaro/victorpafaro/VictorpafaroApplication.java
@@ -0,0 +1,13 @@
+package com.victorpafaro.victorpafaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorpafaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorpafaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorpafaro/victorpafaro/VictorpafaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpafaro.victorpafaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorpafaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
